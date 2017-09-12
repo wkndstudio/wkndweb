62923d1defd6ae0383f44b52606d57704949fc58
designcrumble <y13uc147@lnmiit.ac.in>
2017-09-13 04:34:30 +0530

    smooth scroll + link update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,7 +38,44 @@ function divreveal() {
     	sr.reveal('.section div', { container: sections, duration: 2000, scale: 1, distance: '30px' });
 
 
+//Smooth Scroll & Remove irrevelant links
 
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 // interval and custom config passed to reveal 
 // sr.reveal('section', { duration: 2000 }, 1000);
